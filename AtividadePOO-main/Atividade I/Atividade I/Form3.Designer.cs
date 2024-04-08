f48d166afac006079cf8319fe1dc6cb7ae3cfbf3
PurpleBFox <brunoichikawa9@gmail.com>
2024-04-08 12:18:50 -0300

    Atividade-I---POO

---
+++ AtividadePOO-main/Atividade I/Atividade I/Form3.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Atividade_I
+{
+    partial class frmnadadores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblPergunta = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(667, 392);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(121, 46);
+            this.btnVoltar.TabIndex = 0;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(15, 44);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(190, 20);
+            this.txtDistancia.TabIndex = 1;
+            this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(15, 104);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.ReadOnly = true;
+            this.txtValor.Size = new System.Drawing.Size(190, 20);
+            this.txtValor.TabIndex = 2;
+            // 
+            // lblPergunta
+            // 
+            this.lblPergunta.AutoSize = true;
+            this.lblPergunta.Location = new System.Drawing.Point(12, 9);
+            this.lblPergunta.Name = "lblPergunta";
+            this.lblPergunta.Size = new System.Drawing.Size(146, 13);
+            this.lblPergunta.TabIndex = 3;
+            this.lblPergunta.Text = "Informe a distancia percorrida";
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(12, 77);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(69, 13);
+            this.lblValor.TabIndex = 4;
+            this.lblValor.Text = "Valor Ganho:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(15, 213);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(111, 43);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(132, 213);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(111, 43);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // frmnadadores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblPergunta);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.btnVoltar);
+            this.Name = "frmnadadores";
+            this.Text = "Distancia Natação";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblPergunta;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
---
+++ AtividadePOO-main/Atividade I/Atividade I/Form3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_I
+{
+    public partial class frmnadadores : Form
+    {
+        public frmnadadores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+           frmmenu inicial = new frmmenu();
+            inicial.Show();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double distancia, ganho;
+
+            distancia = Convert.ToDouble(txtDistancia.Text);
+           
+
+             if (distancia < 800)
+            {
+                ganho = 5000.00;
+            }
+             else if(distancia >= 800 &&  distancia <= 1500)
+            {
+                ganho = 10000.00;
+            }
+            else
+            {
+                ganho = 15000.00;
+            }
+
+            txtValor.Text = ganho.ToString();
+        }
+
+        private void txtDistancia_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
